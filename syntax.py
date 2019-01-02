41e61e7a6dbb18e43820675092a62ed479002585
Alex Kim <alexkim@macs-MacBook-Pro-2.local>
2019-01-01 18:56:08 -0600

    added basic functions syntax

---
+++ syntax.py
@@ -0,0 +1,34 @@
+# exponents
+2**4
+
+print("this is for python 3")
+print "this is for python 2"
+
+"""
+multiline
+string
+"""
+
+total_cost = 5
+total_cost += 10
+print(total_cost) #15
+
+def sing_song():
+  print("You may say I'm a dreamer")
+
+
+
+def about_this_computer():
+	print("111")
+print("222")
+
+about_this_computer() # "222" "111"
+
+
+
+
+def create_spreadsheet(title, row_count = 1000):
+  print("Creating a spreadsheet called "+title+" with "+str(row_count)+" rows.")
+
+create_spreadsheet("Downloads") #prints using default 1000
+create_spreadsheet("Applications", 10) #prints using 10
